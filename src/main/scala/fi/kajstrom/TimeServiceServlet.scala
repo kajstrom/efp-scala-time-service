d51731a6588df752e018c98127ca5aaf8f0dfe30
Kaj Ström <kaj.strom@kapsi.fi>
2017-05-15 19:25:06 +0300

    Project setup

---
+++ src/main/scala/fi/kajstrom/TimeServiceServlet.scala
@@ -0,0 +1,16 @@
+package fi.kajstrom
+
+import org.scalatra._
+
+class TimeServiceServlet extends EfpScalaTimeServiceStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
